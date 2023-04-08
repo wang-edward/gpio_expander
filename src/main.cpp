feb6700eed78f60c3f40e70bcc198f573a2e268a
wang-edward <edawordwang@gmail.com>
2023-04-08 16:40:50 -0400

    working 3 ics

--- src/main.cpp
+++ src/main.cpp
@@ -4,83 +4,34 @@
 
 
 
-CAT9555 cat000 = CAT9555(0b00100000);
-// CAT9555 cat001 = CAT9555(0b00100001);
-// CAT9555 cat010 = CAT9555(0b00100010);
-
-// CAT9555 cat010 = CAT9555(0b01000000);
-
+CAT9555 cat0 = CAT9555(0b00100000);
+CAT9555 cat1 = CAT9555(0b00100001);
+CAT9555 cat2 = CAT9555(0b00100010);
 
 void setup() {
   Serial.begin(9600);
-
-Serial.println(cat9555.address);
-  Serial.println(cat000.address);
-  // Serial.println(cat001.address);
-  // Serial.println(cat010.address);
-
-
-  Serial.println("1");
-
   Wire.begin();
-
-  Serial.println("2");
   
-  cat000.begin();
-  Serial.println("3");
-
-  // cat001.begin();
-  
-  Serial.println("4");
-  // cat010.begin();
-  
-
+  cat0.begin();
+  cat1.begin();
+  cat2.begin();
 }
 
 
 void loop() {
-
-  Serial.println("bro");
+	uint32_t c0port0 = cat0.read_8_Register(INPUT_PORT0);
+	uint32_t c0port1 = cat0.read_8_Register(INPUT_PORT1);
   
-  // delay(300);
+	uint32_t c1port0 = cat1.read_8_Register(INPUT_PORT0);
+	uint32_t c1port1 = cat1.read_8_Register(INPUT_PORT1);
 
+	uint32_t c2port0 = cat2.read_8_Register(INPUT_PORT0);
+	uint32_t c2port1 = cat2.read_8_Register(INPUT_PORT1);
 
-  // Serial.println(cat9555.address);
-  // Serial.println(cat000.address);
-  // Serial.println(cat001.address);
+  Serial.print("cat0: "); Serial.print(c0port0,BIN); Serial.println(c0port1,BIN);
+  Serial.print("cat1: "); Serial.print(c1port0,BIN); Serial.println(c1port1,BIN);
+  Serial.print("cat2: "); Serial.print(c2port0,BIN); Serial.println(c2port1,BIN);
 
-
-	uint32_t port0 = cat000.read_8_Register(INPUT_PORT0);
-	uint32_t port1 = cat000.read_8_Register(INPUT_PORT1);
-  
-  // uint32_t port3 = cat001.read_8_Register(INPUT_PORT0);
-  // uint32_t port4 = cat001.read_8_Register(INPUT_PORT1);
-
-  // uint32_t port5 = cat010.read_8_Register(INPUT_PORT0);
-  // uint32_t port6 = cat010.read_8_Register(INPUT_PORT1);
-
-  Serial.print("port0: ");Serial.println(port0,BIN);
-  Serial.print("port1: ");Serial.println(port1,BIN);
-  // Serial.print("port3: ");Serial.println(port3,BIN);
-  // Serial.print("port4: ");Serial.println(port4,BIN);
-  // Serial.print("port5: ");Serial.println(port5,BIN);
-  // Serial.print("port6: ");Serial.println(port6,BIN);
-
-  // delay(10);
-
-	// uint32_t port1 = cat9555.read_16_Register(INPUT_PORT1);
-
-
-  
-  // Serial.println(":ASDASD");
   Serial.println("\n\n");
-
-  // for (int i=0;i<16;i++) {
-  //   Serial.print(i); Serial.print(":  ");
-  //   Serial.println(cat9555.digitalRead(i));
-  // // (cat9555.digitalRead(i));
-
-  // }
-
-  // Serial.println("\n\n\n\n");
+  delay(1000);
 }
